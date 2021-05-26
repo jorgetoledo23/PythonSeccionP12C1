fa3d6c9ccedc50d0888ae43fd96d1b9e0639f2d0
jorgetoledo23 <jorge.toledo.6@hotmail.com>
2021-05-26 13:03:48 -0400

    ejercicio5

--- ejercicio4.py
+++ ejercicio4.py
@@ -3,3 +3,12 @@
 # Si es que tiene hijos entonces que ingrese la edad de cada uno 
 # y asignar un bono de $25.000 por cada hijo de entre 3 y 15 años. 
 # Al final mostrar el total de dinero que obtendría en bono
+
+cantHijos = int(input("Ingrese cantidad de hijos: "))
+totalBono = 0
+for hijo in range(1, cantHijos+1):
+    edad = int(input(f"Ingrese la edad del Hijo N{hijo}: "))
+    if edad >= 3 and edad <= 15:
+        totalBono += 25000
+    else: print("Este hijo no tiene derecho a BONO.")
+print(f"Total a Pagar: {totalBono}")
---
+++ ejercicio5.py
@@ -0,0 +1,3 @@
+#Calcular el promedio de edades de hombres, mujeres 
+# y de todo un grupo de alumnos.
+#Ingresa la cantidad de Alumnos y se pregunta por sexo y edad.
