34b214a8c96ccd4f98a7a678b9c4cc17c7f7b781
jorgetoledo23 <jorge.toledo.6@hotmail.com>
2021-05-31 12:04:57 -0400

    Clase31Mayo

---
+++ ejercicio6.py
@@ -0,0 +1,16 @@
+#Un obrero necesita calcular su salario semanal, el cual se obtiene de la sig. manera:
+#Si trabaja 40 horas o menos se le paga $1600 por hora
+#Si trabaja más de 40 horas se le paga $1600 por cada una de las primeras 40 horas y $2000 por cada hora extra.
+
+print(not(6 > 3))
+
+#             V          
+if not(6 > 3 or 5 < 3):
+    print("Algo")
+else:
+    print("Otra Cosa")
+
+while not(6 > 3 and 7 < 9):
+    print("Algo")
+
+
--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-#             0          1       2
+while True:
+    nota = int(input("Ingrese una Nota (1 - 7): "))
+    if  nota >= 1 and nota <= 7:
+        print(nota)
+        break
 
-for nota in range(1, 10):
-    print(nota)
+print("Hola")
+
